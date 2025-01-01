b4c580f5adcc4fc8895ee61d3a94559e4c331065
Paulo Bandua <pcbandua@gmail.com>
2025-01-01 15:32:09 -0800

    addded Header, Footer, and Products Page in React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { ProductsPage } from "./ProductsPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <PhotosPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
