d0216cf7786bae18591cf248ac55a310e1a8485a
Paulo Bandua <pcbandua@gmail.com>
2025-04-02 17:39:12 -0700

    added Header tailwind

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,20 @@
 export function Footer() {
   return (
-    <footer>
-      <p>Copyright 2022</p>
+    <footer className="bg-gray-800 text-white text-center p-4">
+      <div className="max-w-7xl mx-auto px-4 py-8">
+        <div className="flex flex-col md:flew-row justify-between items-center">
+          <div>
+            <p className="text-lg">Art Gallery</p>
+          </div>
+          <div>
+            <p className="text-lg">Contact Us</p>
+          </div>
+          <div>
+            <p className="text-lg">About Us</p>
+          </div>
+        </div>
+        <p>Copyright 2022</p>
+      </div>
     </footer>
   );
 }
